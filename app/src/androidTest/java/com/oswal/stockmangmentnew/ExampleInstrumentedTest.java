793ea184c22e4f22ec1a449bb0fb383820f9d7d3
oswal50 <43981630+oswal50@users.noreply.github.com>
2019-08-20 00:29:28 +0530

    20thAug2019 2nd Time

--- app/src/androidTest/java/com/oswal/stockmangmentnew/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/oswal/stockmangmentnew/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.com.oswal.stockmangmentnew", appContext.getPackageName());
+        assertEquals("com.oswal.stockmangmentnew", appContext.getPackageName());
     }
 }
